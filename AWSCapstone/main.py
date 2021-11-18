2ef0639111c92268620403cc1010f2eca4173604
Shreyas Raj Singh <sxs170100@utdallas.edu>
2021-11-18 01:47:24 -0600

    Revert "Search API made"
    This reverts commit de4edcf6227185209cc46daefa21a89396cba033.

--- AWSCapstone/main.py
+++ AWSCapstone/main.py
@@ -28,33 +28,24 @@ for row in table.findAll('tr')[1:]:
     if count == 100:
         break
     
-# heads = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
-# "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
-# z = []
-# for ticker in ticker_to_cik.keys():
-#     main_req = "https://data.sec.gov/api/xbrl/companyfacts/CIK" + ticker_to_cik[ticker] + ".json"
-#     resp = requests.get(main_req, headers=heads)
-#     x = resp.text
+heads = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
+"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
 
-#     # parse x:
-#     y = json.loads(x)
+for ticker in ticker_to_cik.keys():
+    main_req = "https://data.sec.gov/api/xbrl/companyfacts/CIK" + ticker_to_cik[ticker] + ".json"
+    resp = requests.get(main_req, headers=heads)
+    x = resp.text
 
-#     # for key in y["facts"]["us-gaap"].keys():
-#     #     z.append({
-#     #         "attrb": key
-#     #     })
+    # parse x:
+    y = json.loads(x)
 
-#     # print(z)
+    # the result is a Python dictionary:
+    outfile = "parsed_json_files/" + str(y["cik"]) + "_cik" + ".json"
 
-#     # break
+    with open(outfile, 'w') as json_file:
+        json.dump(y, json_file)
 
-#     # the result is a Python dictionary:
-#     outfile = "parsed_json_files/" + str(y["cik"]) + "_cik" + ".json"
-
-#     with open(outfile, 'w') as json_file:
-#         json.dump(y, json_file)
-
-#     print("Parsing complete for: " + outfile)
+    print("Parsing complete for: " + outfile)
 
 
 # for key in it.keys():
@@ -71,21 +62,3 @@ for row in table.findAll('tr')[1:]:
 # for ticker in it["Information Technology"]:
 #     obj.get("10-K", ticker, amount=1)
 #     obj.get("8-K", ticker, amount=1)
-
-tc = []
-
-for t in ticker_to_cik.keys():
-    tc.append({
-        "name": str(t),
-        "cik": str(ticker_to_cik[t])
-    })
-
-# with open("/Users/srs1029/Desktop/AWS-Contextual-Voice-Search/front-ui/public/json/attribute.json", 'r') as json_file:
-#     tc = json.loads(json_file.readlines())
-
-#     print([item["attrb"] for item in tc])
-
-with open("/Users/srs1029/Desktop/AWS-Contextual-Voice-Search/parsed_json_files/1800_cik.json", 'r') as json_file:
-    tc = json.load(json_file)
-
-    print([key for key in tc["facts"]["us-gaap"].keys()])
