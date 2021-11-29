2809a149f03102a062e8c1980efa716461d8337c
Shreyas Raj Singh <sxs170100@utdallas.edu>
2021-11-28 23:44:48 -0600

    Added API code

--- API/app.py
+++ API/app.py
@@ -4,41 +4,80 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/<cik>")
-def data(cik):
+# @app.route("/test")
+# def test():
+#     return "Server works"
 
-    ret = {}
+# @app.route("/compDict")
+# def data(cik):
 
-    file_name = "../parsed_json_files/" + cik + "_cik.json"
+#     ret = {}
 
-    with open(file_name, "r") as js:
-        data = json.load(js)
+#     file_name = "../parsed_json_files/" + cik + "_cik.json"
+
+#     with open(file_name, "r") as js:
+#         data = json.load(js)
     
-    ret = data["facts"]["us-gaap"]
+#     ret = data["facts"]["us-gaap"]
 
-    return jsonify(ret)
+#     return jsonify(ret)
 
-@app.route("/<cik>/target")
-def data2(cik):
+# @app.route("/test1/<cik>/target?")
+# def data2(cik):
+
+#     args = request.args
+
+#     ret = []
+
+#     file_name = "../parsed_json_files/" + cik + "_cik.json"
+
+#     with open(file_name, "r") as js:
+#         data = json.load(js)
+    
+#     if len(args) == 0:
+#         return jsonify(data)
+#     elif len(args) >= 1:    
+#         for key in args.keys():
+#             attrb = args[key]
+#             ret.append(data["facts"]["us-gaap"][attrb])
+
+
+#     return jsonify(ret)
+
+# 
+# attrb = <attribute> ; year = <year>
+# 
+@app.route("/<cik>/<attrb>/<year>")
+def data3(cik, attrb, year):
+
+    # c = company.split("#")
+    # company = " ".join(c)
 
     args = request.args
 
     ret = []
 
+    # # load company name to cik mapping
+    # c_to_cik = {} 
+    # file_name = "static/companyName_to_cik.json" 
+    # with open(file_name, "r") as js:
+    #     c_to_cik = json.load(js)
+
+    # cik = c_to_cik[company]
+
     file_name = "../parsed_json_files/" + cik + "_cik.json"
 
     with open(file_name, "r") as js:
         data = json.load(js)
-    
-    if len(args) == 0:
-        return jsonify(data)
-    elif len(args) >= 1:    
-        for key in args.keys():
-            attrb = args[key]
-            ret.append(data["facts"]["us-gaap"][attrb])
-
+       
+         
+    for entry in data["facts"]["us-gaap"][attrb]["units"]["USD"]:
+        filed_date = entry["filed"]
+        y, m, d = filed_date.split("-")
+        if int(y) == int(year):
+            ret.append(entry)
 
     return jsonify(ret)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=5000)
